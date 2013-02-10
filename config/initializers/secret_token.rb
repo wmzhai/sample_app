5e9c7435a81b3d733986313218ef59e8e4940b45
Weiming Zhai <wmzhai@gmail.com>
2013-02-11 07:18:30 +0900

    初始化创建sample_app (RORT ch3.0) $rails new sample_app --skip-test-unit Here the --skip-test-unit option to the rails command tells Rails not to generate a test directory associated with the default Test::Unit framework. This is not because we won’t be writing tests; on the contrary, starting in Section 3.2 we will be using an alternate testing framework called RSpec to write a thorough test suite.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '334ca5306ab7dabbc94f167d988f94d9dc5f3779a39e719f40806665d2fa00fef0b57cd2e2cabf605fd48804b9d76686f8e20520481627b0c5ae433b379b61f9'
