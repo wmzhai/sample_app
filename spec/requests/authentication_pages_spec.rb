db3b3fc641e7f1e3fdb5fd8033c142665c9b113c
Weiming Zhai <wmzhai@gmail.com>
2013-02-14 18:18:04 +0800

    添加用户列表的显示 ch9.3.1 User index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,10 +4,28 @@ describe "Authentication" do
 
   subject { page }
 
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
+
+    it {should have_selector('title', text: 'All users')}
+    it {should have_selector('h1', text: 'All users')}
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector('li', text: user.name)
+      end
+    end
+  end
+
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
 
@@ -29,15 +47,18 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
+        fill_in "Email", with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users', href: users_path)}
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
+
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -46,7 +67,6 @@ describe "Authentication" do
       end
     end
   end
-
 
   describe "authorization" do
 
@@ -78,6 +98,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visit the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
     end
