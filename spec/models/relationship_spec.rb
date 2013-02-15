0c412688ab689e7ddb91e04b6626633941fb22cc
Weiming Zhai <wmzhai@gmail.com>
2013-02-15 22:37:36 +0800

    ch11.1.4 Followed users

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -18,12 +18,21 @@ describe Relationship do
     end
   end
 
-
   describe "follower methods" do
     it { should respond_to(:follower) }
     it { should respond_to(:followed) }
     its(:follower) { should == follower }
     its(:followed) { should == followed }
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
   end
 end
 
